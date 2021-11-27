eb32959ea014916c53cc8a9c6fe06364e82ebabf
steveblus <30328053+steveblus@users.noreply.github.com>
2021-11-27 12:15:28 +0100

    AddIDP
    Add new IDP project

---
+++ DouceSody.WebUIWithIdp/Data/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace DouceSody.WebUIWithIdp.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime BirthDay { get; set; }
+
+        public string? FullName { get; set; }
+    }
+}
+
---
+++ DouceSody.WebUIWithIdp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DouceSody.WebUIWithIdp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
